57e4bab7c3fd277dc033766d9d7d651344bd93ab
Ankit Soni <ankit.soni05@gmail.com>
2021-06-19 18:33:32 +0530

    SomeMoreDirectives

--- moshTuts/src/app/directives/directives.component.ts
+++ moshTuts/src/app/directives/directives.component.ts
@@ -8,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent implements OnInit {
 
   courses = ['c1','c2','c3'];
+  tasks = {
+  totaltasks : ['T1','T2','T3','T4','T5'],
+  activetasks : ['A1','A2','A3','A4','A5','A6','A7'],
+  inactivetasks : ['I1','I2','I3']
+  }
+  ActiveStatus = "tt";
+  contentActiveClass = "show active";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeStatus(currentStatus :string){
+    this.ActiveStatus = currentStatus;
   }
 
 }
