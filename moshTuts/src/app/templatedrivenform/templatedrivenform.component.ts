62f0601a5f49f55072bb316b1e20266529902cac
Ankit Soni <ankit.soni05@gmail.com>
2021-06-26 19:52:57 +0530

    template driven form commit 2

--- moshTuts/src/app/templatedrivenform/templatedrivenform.component.ts
+++ moshTuts/src/app/templatedrivenform/templatedrivenform.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplatedrivenformComponent implements OnInit {
 
+  cities = 
+  [
+    {id:1,name:"Nagpur"},
+    {id:2,name:"Pune"},
+    {id:3,name:"Mumbai"},
+    {id:4,name:"Banglore"},
+    {id:5,name:"Hyderabad"}
+  ];
+  genders = 
+  [
+    {id:1,name:"Male"},
+    {id:2,name:"Female"},
+    {id:3,name:"N/A"},
+  ];
+  finalisedJSON :any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(f:any)
+  {
+    console.log(f);
+    this.finalisedJSON = f.value;
+  }
 }
